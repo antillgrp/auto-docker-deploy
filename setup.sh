50b717ebfdab4cb84d4a17d4e6a142289724cc4c
antillgrp <antillgrp@gmail.com>
2024-07-28 09:27:17 -0400

    deploy-certscan-docker-1.0.0.sh small fixes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# wget -qO- https://git.new/rfR0pfn | bash
+
 #set -eu -o pipefail # fail on error and report it, debug all lines
 #sudo -n true # -n, --non-interactive         non-interactive mode, no prompts are used
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
